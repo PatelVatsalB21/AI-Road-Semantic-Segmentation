6d3bc4e57e312a9fa9acd2564d014d2e17c2f486
Vatsal <70195106+PatelVatsalB21@users.noreply.github.com>
2021-07-31 10:35:43 +0530

    Updated hyperparameters

--- model/eval/model_train.py
+++ model/eval/model_train.py
@@ -15,10 +15,10 @@ keras.backend.clear_session()
 model = get_model()
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 callbacks = [
-    ReduceLROnPlateau(monitor="val_loss", patience=3, factor=0.5, verbose=1, min_lr=1e-12),
+    ReduceLROnPlateau(monitor="val_loss", patience=5, factor=0.1, verbose=1, min_lr=1e-12),
 ]
 
-history = model.fit(input_images, mask_images, batch_size=32, epochs=30, callbacks=callbacks, validation_split=0.1)
+history = model.fit(input_images, mask_images, batch_size=32, epochs=50, callbacks=callbacks, validation_split=0.1)
 
 plt.figure()
 plt.plot(history.history['accuracy'], label='Training')
